506c1fc313615a8712bbbea3e4f9d9d084163ae2
shojishohei <59309409+shojishohei@users.noreply.github.com>
2019-12-30 02:34:17 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,18 +1,38 @@
+$(function () {
 
-$(function(){
-  $('.section-inner')
-     .on('mouseover',function(){
-       $('.section-inner').stop(true).animate({
-         backgroundColor:'#ae5e9b'
-       },
-       500
-     );
-  })
-     .on('mouseout',function(){
-       $('.section-inner').stop(true).animate({
-         backgroundColor:'#3498db'
-       },
-       500
-     );
+    /*
+     * Slideshow
+     */
+    // slideshow クラスを持った要素ごとに処理を実行
+    $('.slideshow').each(function () {
+
+        var $slides = $(this).find('img'), // すべてのスライド
+            slideCount = $slides.length,   // スライドの点数
+            currentIndex = 0;              // 現在のスライドを示すインデックス
+
+        // 1 番目のスライドをフェードインで表示
+        $slides.eq(currentIndex).fadeIn();
+
+        // 7500 ミリ秒ごとに showNextSlide 関数を実行
+        setInterval(showNextSlide, 4000);
+
+        // 次のスライドを表示する関数
+        function showNextSlide () {
+
+            // 次に表示するスライドのインデックス
+            // (もし最後のスライドなら最初に戻る)
+            var nextIndex = (currentIndex + 1) % slideCount;
+
+            // 現在のスライドをフェードアウト
+            $slides.eq(currentIndex).fadeOut();
+
+            // 次のスライドをフェードイン
+            $slides.eq(nextIndex).fadeIn();
+
+            // 現在のスライドのインデックスを更新
+            currentIndex = nextIndex;
+        }
+
     });
-  });
+
+});
