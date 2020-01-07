f8cec2609111f16f37c5f4c4c20ecb72e868536e
shojishohei <59309409+shojishohei@users.noreply.github.com>
2020-01-08 00:11:51 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -173,16 +173,7 @@ $(window).scroll(function (){
            }
        });
    });
-   $(window).scroll(function (){
-      $('.fadein2').each(function(){
-          var elemPos = $(this).offset().top,
-              scroll = $(window).scrollTop(),
-              windowHeight = $(window).height();
-            if (scroll > elemPos - windowHeight + 150){
-                $(this).addClass('scrollin');
-              }
-          });
-      });
+
       $(window).scroll(function (){
          $('.fadein3').each(function(){
              var elemPos = $(this).offset().top,
